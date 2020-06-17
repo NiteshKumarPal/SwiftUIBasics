5a410c264453dea67008837953f258789862339f
nitesh-iosdev <nitesh@markmyground.com>
2020-06-17 21:41:37 +0530

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by Nitesh Kumar Pal on 17/06/20.
+//  Copyright © 2020 Initiator. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
